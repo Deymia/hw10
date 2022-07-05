341bf80771f5ee7c20dd24e673c5420d1a7e65db
Anastasia <shvayka92@mail.ru>
2022-07-05 20:54:10 +0300

    done

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Book {
-}
+import java.util.Objects;
+
+public class Book {
+        private String bookName;
+        private String author;
+        private int publicYear;
+
+        public Book(String bookName, String author, int publicYear) {
+                this.bookName = bookName;
+                this.author = author;
+                this.publicYear = publicYear;
+        }
+
+        public String getBookName() {
+                return this.bookName;
+        }
+
+        public String getAuthor() {
+                return this.author;
+        }
+
+        public int getPublicYear() {
+                return this.publicYear;
+        }
+
+        public void setPublicYear(int publicYear) {
+                this.publicYear = publicYear;
+        }
+
+        public String toString() {
+                return "Название: " +this.bookName + "\n Автор: " + this.author + "\n Год публикации: " + this.publicYear;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Book book = (Book) o;
+                return bookName.equals(book.bookName);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(bookName);
+        }
+
+        }
